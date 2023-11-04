1c1b07286e26348f8b242f4a4ca85d11fb44da80
Mario <mario@linuxmex.com>
2023-11-04 09:06:36 +0100

    Analizar HTML recuperar datos

---
+++ web/cap2_exo29.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from html.parser import HTMLParser
+import sys 
+
+class TitleParser(HTMLParser):
+    def __init__(self):
+        self.title = ""
+        self.readingtitle = 0
+        HTMLParser.__init__(self)
+
+    def handle_starting(self, tag, attrs):
+        if tag == 'title':
+            self.readingtitle = 1 
+
+    def handle_data(self, data):
+        if self.readingtitle:
+            self.title += data 
+    
+    def handle_endtag(self, tag):
+        if tag == "title":
+            self.readingtitle = 0 
+
+    def gettitle(self):
+        return self.title 
+    
+fd = open(sys.argv[1])
+tp = TitleParser()
+tp.feed(fd.read())
+# Obtener los datos de cap2_HTML.html
+print("El título es: ", tp.gettitle())
