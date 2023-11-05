c68e01840463331e52ee12d8fa20aee7cb670556
Mario <mario@linuxmex.com>
2023-11-05 18:10:59 +0100

    Updated last changes in comments

--- email/cap2_exo35.py
+++ email/cap2_exo35.py
@@ -1,56 +1,24 @@
-#!/usr/bin/env python 
+# Se tiene que introducir el archivo email.txt: 
+# $cap2_exo35.py < email.txt
+#!/usr/bin/env python
 
-import smtplib
+import sys, email 
 
-# Un email muliparte (contiene anexos)
-from email.mime.multipart import MIMEMultipart
+msg = email.message_from_file(sys.stdin)
+print("*** Cabeceras del mensaje: ")
+for header, value in msg.items():
+    print(header + ":")
+    print(" " + value)
 
-# Un mensaje email de tipo texto 
-from email.mime.text import MIMEText
+if msg.is_multipart():
+    print("Este programa no soporta los correos multipart")
+    sys.exit(1)
 
-# Un mensaje email de tipo imagen
-from email.mime.image import MIMEImage
+print("-" * 78)
+if 'subject' in msg:
+    print("Asunto: ", msg['subject'])
+    print("-" * 78)
 
-import mimetypes, posixpath 
-
-def image_a_mail(rutaarchivo):
-    '''envío de un mensaje de tipo MIMEImage a partir'''
-# Utilizamos posix para tener el nombre del archivo
-    nomarchivo = posixpath.basename(rutaarchivo)
-# luego obtenermos la extensión del archivo 
-    extension = posixpath.splitext(nomarchivo)
-# Luego usamos mimetypes para tener el content-type de la extensión 
-    content_type = mimetypes.types_map[extension[1]]
-# Abrimos el archivo de imagen en modo binario 
-    archivo = open(rutaarchivo, 'rb')
-# Un objeto message con el contenido del archivo
-    image = MIMEImage(archivo.read())
-# Añadimos las cabeceras para la imagen
-    image.add_header('Content-Disposition', 'attachment;filename="%s"'%nomarchivo)
-    image.add_header("Content-Type", content_type)
-# Devolvemos el objeto message que contiene la imagen
-    return image 
-
-def send(mfrom, mto):
-    #Añadimos las cabeceras para el mail principal 
-    emailmultipart['From'] = mfrom 
-    emailmultipart['To'] = mto 
-    emailmultipart['Subject'] = '¡Hola!'
-    # Creamos un mensaje simple en HTML (¡Qué nivel!;)
-    emailtext = MIMEText('<b>¡Hola!</b>', 'html')
-    # agregamos este mail a nuestro multipart 
-    emailmultipart.attach(emailtext)
-
-# Creamos un objeto mensaje multipart
-emailmultipart = MIMEMultipart()
-# Creamos un mensaje de tipo MIMEImage empleando nuestra función
-emailimage = image_a_mail("/home/rutausuario/otra-ruta/imagen1.png")
-# Vinculamos la imagen a nuestro multipart
-emailmultipart.attach(emailimage)
-
-# Enviamos el mail
-server = smtplib.SMTP('smtp.linuxmedellin.org', 587)
-send('mario@correo.com', 'maria@correo.es')
-# Agregamos el mail a nuestro multipart 
-server.sendmail('mario@correo.com', 'maria@correo.es', emailmultipart.as_string())
-server.quit()
+print("Cuerpo del mensaje:")
+print("")
+print(msg.get_payload())
